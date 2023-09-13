66b6127fc6b8508c9bb84dcc28ebe4b67cbf4235
Lottaltz <namhwan210754@gmail.com>
2023-09-13 16:59:07 +0700

    edit image

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -7,7 +7,7 @@ const Owner = () => {
         <Layout>
             <div className="owner-page">
                 <h1>Kwan - Group L - 18</h1>
-                <img src="/public/programmer.png" alt="owner page image" />
+                <img src="./programmer.png" alt="owner page image" />
                 <div className="shortbio">
                     <h3>Short Biography:</h3>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque aliquam, molestiae quidem repellat dolores natus error. Accusamus dolorum atque ab odio harum quam voluptate, architecto numquam, magni cupiditate labore voluptas deserunt, consequatur voluptatibus consequuntur maiores asperiores quasi quis. Est, nisi, neque libero natus quisquam possimus accusantium mollitia commodi iusto animi porro maxime, totam amet! Iure doloremque reiciendis, officiis nostrum corporis aut. Maxime nesciunt voluptatibus facilis quam, ullam soluta hic, ratione mollitia exercitationem accusantium deserunt doloribus sed modi corrupti fugiat expedita blanditiis, dolores excepturi animi. Eveniet, molestiae minima! Sed saepe itaque provident, non alias maxime inventore nesciunt voluptatem sunt, numquam autem.</p>
